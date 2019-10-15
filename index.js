8afa0285a63248b0528f3d3c1567edc0168d24fd
Alex Cucos <alexcucos1@gmail.com>
2019-10-14 23:04:26 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ async function initialize(web3){
   ethereum.enable()
 
 
-  provider = new ethers.providers.Web3Provider(web3.currentProvider);
-  utils = ethers.utils;
+  provider = new ethers.providers.Web3Provider(web3.currentProvider)
+  utils = ethers.utils
 
   let accounts = await provider.listAccounts()
   signer = provider.getSigner(accounts[0])
+}
 
-  
+async fucntion send(){
+  let address = document.getElementByID("addressTo")
+  let amount = parseAmount(document.getElementByID("toAmount"))
+
+
 }
